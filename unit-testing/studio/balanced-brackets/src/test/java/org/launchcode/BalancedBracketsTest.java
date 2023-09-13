7f7147699401a0d4dfda156625ff5fd6b89d1cf6
TraNov <tracy.novel@nidec-motor.com>
2023-09-13 16:23:17 -0500

    Unit Testing

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,68 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+//}
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void testStringWithNoBrackets () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello no brackets"));
+    }
+
+    @Test
+    public void strOutsideBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]str"));
+
+    }
+
+    @Test
+    public void testBalancedBrackets1() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void singleRightBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void singleLeftBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testUnbalancedBrackets4() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testUnbalancedBrackets3() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void testUnopenedBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]"));
+    }
+
+    @Test
+    public void testCrazyBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]]]][[]]["));
+    }
+
+    @Test
+    public void balancedBracketsInbetweenCharsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[st]r"));
+    }
+
+
 }
+
